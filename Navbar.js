90d860beb884c10993124869b57fa46ad4f1823c
Pavan Varma <pavanvarma.2725@gmail.com>
2025-08-16 15:00:50 +0530

    add Navbar component

---
+++ Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-white shadow-md p-4">
+      <h1 className="text-xl font-bold text-blue-600">JobConnect</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
